4b78e92b513c65bf79ca52b47294f1294a85ab9e
juanjo <jlopezco@unsa.edu.pe>
2023-03-04 12:16:46 -0500

    Actualizacion tarea 07

--- Curso-Pre/07 - Clases & OOP/ejecucion.py
+++ Curso-Pre/07 - Clases & OOP/ejecucion.py
@@ -1,4 +1,9 @@
-import func
+'''import func
 
 v3=func.funciones()
+print(v3.factorial1(13))
+'''
+# cuando importamos todo ya no es necesarios poner func
+from func import*
+v3=funciones()
 print(v3.factorial1(13))
